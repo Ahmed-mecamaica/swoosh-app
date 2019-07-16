a4bec63d532a5f671d627a6c97a4bbb816ddeff6
maika <maika@trojans-MacBook-Pro.local>
2019-07-16 16:59:55 +0200

    pass vlaue between view controller

--- swoosh-app/swoosh-app/controller/leagueVC.swift
+++ swoosh-app/swoosh-app/controller/leagueVC.swift
@@ -43,14 +43,17 @@ class leagueVC: UIViewController {
     }
     
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+            skillVC.player = player
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
